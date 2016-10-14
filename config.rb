e9f7e4b2d3d51f3b34da9b5105bd74d9c6291e4b
Petr Marek <contact@petrmarek.eu>
2016-10-14 12:40:45 +0200

    wip

--- config.rb
+++ config.rb
@@ -1,8 +1,41 @@
+require 'ostruct'
+
 configure :development do
   activate :livereload
 end
 
 helpers do
+  def yaml_data(yaml_name)
+    struct_array YAML.load(File.read(File.join(root, "source/data/#{yaml_name}.yml")))
+  end
+
+  def work_data(yaml_name)
+    data = yaml_data(yaml_name)
+    data.map do |category|
+      category.projects = struct_array(category.projects)
+      category
+    end
+  end
+
+  def struct_array(array)
+    array.map { |a| OpenStruct.new(a) }
+  end
+
+  def nice_link_to_url(url)
+    return if url.nil?
+    nice_url = url.gsub(%r{https?://}, '').gsub(%r{/$}, '')
+    link_to nice_url, url, target: '_blank'
+  end
+
+  def markdown(text)
+    render_options = {
+      hard_wrap: true,
+      link_attributes: { target: '_blank' }
+    }
+    extensions = { autolink: true }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
 
 activate :sprockets
--- source/javascripts/_page_nav.coffee
+++ source/javascripts/_page_nav.coffee
@@ -2,16 +2,18 @@ pageNav = ->
   body = document.getElementById('m-body')
   handle = ->
     hash = window.location.hash.split('#').pop()
+    classes = body.className.replace(/\s*m-show-\w+/, '')
     if ['contact', 'about', 'work'].indexOf(hash) is -1
-      body.classList = []
       window.location.hash = ''
     else
-      body.classList = ["m-show-#{hash}"]
+      classes += " m-show-#{hash}"
+    body.className = classes
 
   $(window).on 'hashchange', handle
   handle()
 
-  $('#m-body').on 'click', (e) -> window.location.hash = ''
   $('.m-section > .container, .m-menu').on 'click', (e) -> e.stopPropagation()
+
+  setTimeout (-> body.className += ' m-loaded'), 0
 
 $ pageNav
